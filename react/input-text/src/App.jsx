99b6ca7101fda0099cf7e7e4adbce85f14156b0b
Thusara Senanayake <t89896736+thusarasenanayake@users.noreply.github.com>
2024-04-19 06:29:43 +0530

    modified:   src/App.jsx

--- react/input-text/src/App.jsx
+++ react/input-text/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -6,11 +6,19 @@ import "./App.css";
 function App() {
   const [name, setName] = useState("");
 
+  useEffect(function () {
+    console.log("use effect");
+  }, []);
+
   return (
     <>
       <h2>Hello, {name}</h2>
 
       <input type="text" onChange={(eve) => setName(eve.target.value)} />
+
+      <br />
+      <br />
+      <code>#{new Date().getTime()}</code>
     </>
   );
 }
